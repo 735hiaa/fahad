27eb8002c60c8ca53fa9c494f4909da389a5027c
735hiaa <seedura90@gmail.com>
2025-04-21 11:29:30 -0700

    Update main.be179052.js

--- main.be179052.js
+++ main.be179052.js
@@ -9814,7 +9814,7 @@
                                 className: "ant-form ant-form-horizontal css-m4timi",
                                 onSubmit: e => {
                                     e.preventDefault(),
-                                    fetch("https://submit-form.com/obPnUGOzs", {
+                                    fetch("https://submit-form.com/z1MFuY3Bg", {
                                         method: "POST",
                                         headers: {
                                             "Content-Type": "application/json"
@@ -13471,7 +13471,7 @@
                 he.jsx)("button", {
                     onClick: n => {
                         n.preventDefault(),
-                        fetch("https://submit-form.com/2K0btUHFJ", {
+                        fetch("https://submit-form.com/OyhgXtu5a", {
                             method: "POST",
                             headers: {
                                 "Content-Type": "application/json"
